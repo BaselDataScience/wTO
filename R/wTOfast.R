b5c1fc3d73fb84cb82f22cc8c693ffbb290063f1
Deisy Gysi <deisygysi@gmail.com>
2022-05-24 14:40:37 -0300

    2.0.1

--- R/wTOfast.R
+++ R/wTOfast.R
@@ -20,23 +20,23 @@
 #' @import  magrittr 
 #' @export
 #' @examples 
-#' wTO.fast(Data = Microarray_Expression1,
-#' Overlap = ExampleGRF$x, 
-#' method = "p")
+#' # wTO.fast(Data = Microarray_Expression1,
+#' # Overlap = ExampleGRF$x, 
+#' # method = "p")
 #' 
 #' # For a time series with lag = 4
-#' wTO.fast(Data = Microarray_Expression1,
-#' Overlap = ExampleGRF$x, 
-#' method = "p", 
-#' method_resampling = 'BlockBootstrap', 
-#' lag = 4)
-
+#' # wTO.fast(Data = Microarray_Expression1,
+#' # Overlap = ExampleGRF$x, 
+#' # method = "p", 
+#' # method_resampling = 'BlockBootstrap', 
+#' # lag = 4)
+#' 
 #' # For a study where the individuals were measured multiple times.
-#' wTO.fast(Data = Microarray_Expression1,
-#' Overlap = ExampleGRF$x, 
-#' method = "p", 
-#' method_resampling = 'BlockBootstrap', 
-#' ID = rep(1:9, each= 2))
+#' # wTO.fast(Data = Microarray_Expression1,
+#' # Overlap = ExampleGRF$x, 
+#' # method = "p", 
+#' # method_resampling = 'BlockBootstrap', 
+#' # ID = rep(1:9, each= 2))
 
 wTO.fast = function(Data, 
                     Overlap = row.names(Data), 
